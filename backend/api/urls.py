cfb7aa0d658662312ed8ff3c5c4debc2315da8d1
Patrick Gallardo <mooltoo.bot@gmail.com>
2025-02-11 08:43:18 +0800

    Created api route for file upload and file url

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from . import views
+from .views import upload_doc_file, upload_doc_fileurl
 
-# urlpatterns = [
-#     path("upload_file/"),
-#     path("upload_fileUrl/"),
-# ]
+urlpatterns = [
+    path("upload-file/", upload_doc_file, name="upload_file"),
+    path("upload-fileurl/", upload_doc_fileurl, name="upload_fileurl"),
+]
