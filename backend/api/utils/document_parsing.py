573d7a2a80892d5141c672c99f9a8c21889db2f8
Patrick Gallardo <mooltoo.bot@gmail.com>
2025-02-11 14:38:42 +0800

    Refactored parsing function used threads

--- backend/api/utils/document_parsing.py
+++ backend/api/utils/document_parsing.py
@@ -1,66 +1,68 @@
-import requests
+from rest_framework.response import Response
 from io import BytesIO
 from paddleocr import PaddleOCR
 from pdf2image import convert_from_bytes
+from dotenv import load_dotenv
+from concurrent.futures import ThreadPoolExecutor
+from backend.status_code import STATUS_CODES, STATUS_MESSAGES
+
 import numpy as np
 import cv2
-from dotenv import load_dotenv
+import requests
 import os
+import time
+
 # from app.utils.openai.create_template import create_template
 load_dotenv('.env')
 
 # Initialize PaddleOCR
-ocr = PaddleOCR(use_angle_cls=True, lang='en', rec_algorithm='CRNN',
+ocr = PaddleOCR(lang='en', rec_algorithm='CRNN',
                 det_db_box_thresh=0.6, det_db_unclip_ratio=1.5)
 
 
-def download_file(request):
-    data = request.json
-    if 'fileUrl' not in data:
-        print("No file URL provided")
-        return "No file URL provided"
-
-    file_url = data['fileUrl']
-
+def download_file(file_url):
     try:
         response = requests.get(file_url, timeout=10)
         if response.status_code != 200:
-            print("Failed to download the file")
-            return "Failed to download the file"
+            return Response({"message": f'{STATUS_MESSAGES["errors"]["FAILED_DOWNLOAD"]}'},
+                     status=STATUS_CODES["errors"][400])
         
         content_type = response.headers.get('Content-Type', '')
         if 'pdf' not in content_type:
-            print("Only PDF files are supported")
-            return "Only PDF files are supported"
-        
+            return Response({"message": f'{STATUS_MESSAGES["errors"]["UNSUPPORTED_FILE_FORMAT"]}'},
+                     status=STATUS_CODES["errors"][415])
         file = BytesIO(response.content)
         return file
     except Exception as e:
-        return str(e)
+        return Response(
+            {"message": f"{str(e)}"},
+            status=STATUS_CODES["errors"][500]
+        )
     
 
+def process_page(page):
+    """Process a single page using OCR and extract text."""
+    img = cv2.cvtColor(np.array(page), cv2.COLOR_RGB2BGR)
+    result = ocr.ocr(img, cls=True)
+
+    return '\n'.join(word_info[0] for line in result[0] for word_info in line if isinstance(word_info[0], str))
+
+
 def doc_parse(file):
+    start_time = time.time()
     document_template = ''
     pdf_bytes = file.read()
-    POPPLER_PATH = os.getenv('POPPLER_PATHS')
+
+    POPPLER_PATH = os.getenv('POPPLER_PATH')
 
     # Convert PDF to images
-    pages = convert_from_bytes(pdf_bytes, dpi=300, poppler_path=POPPLER_PATH)
+    pages = convert_from_bytes(pdf_bytes, dpi=200, poppler_path=POPPLER_PATH)
 
     extracted_text = ''
-    for page in pages:
-        # Convert the PIL image to OpenCV format (required by PaddleOCR)
-        img = cv2.cvtColor(np.array(page), cv2.COLOR_RGB2BGR)
-
-        # Run OCR
-        result = ocr.ocr(img, cls=True)
-
-        # Process and extract text from the result
-        for line in result[0]:
-            for word_info in line:
-                if isinstance(word_info[0], str):
-                    if document_template == '':
-                        document_template = create_template(word_info[0])
-                    extracted_text += word_info[0] + '\n'
-    return extracted_text, document_template
+    with ThreadPoolExecutor() as executor:
+        extracted_text = executor.map(process_page, pages)
+    end_time = time.time() 
+    elapsed_time = end_time - start_time
+    print(f"Document parsing took {elapsed_time:.2f} seconds.")
+    return extracted_text
 
