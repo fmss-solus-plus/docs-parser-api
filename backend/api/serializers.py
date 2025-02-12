75ddaed642b658796f4094b03ffa3733ebab4467
Patrick Gallardo <mooltoo.bot@gmail.com>
2025-02-12 13:11:12 +0800

    Added serializers for checking document types

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from django.core.validators import URLValidator
+import api.utils.openai_api.prompt_template as prompts
 
 class DocumentUploadSerializer(serializers.Serializer):
     file = serializers.FileField(
@@ -7,9 +8,21 @@ class DocumentUploadSerializer(serializers.Serializer):
         help_text="Document file to upload"
     )
 
+    document_type = serializers.ChoiceField(
+        choices=[(key, key) for key in prompts.RECOMMENDED_DOCUMENTS_LISTS.keys()],  # Converts dict keys into choice tuples
+        required=True,
+        help_text="Select the document type"
+    )
+
 class DocumentUrlSerializer(serializers.Serializer):
     file_url = serializers.URLField(
         required=True,
         validators=[URLValidator(schemes=['http', 'https'])],
         help_text="URL of the document to process"
+    )
+
+    document_type = serializers.ChoiceField(
+        choices=[(key, key) for key in prompts.RECOMMENDED_DOCUMENTS_LISTS.keys()],  # Converts dict keys into choice tuples
+        required=True,
+        help_text="Select the document type"
     )
