62f8e9dfd3deecc10d9f1c686934eadfd5ceee56
Patrick Gallardo <mooltoo.bot@gmail.com>
2025-02-13 10:08:30 +0800

    updated prompts

--- backend/api/utils/openai_api/prompt_template.py
+++ backend/api/utils/openai_api/prompt_template.py
@@ -1,13 +1,16 @@
 SYSTEM_TEMPLATE = '''
-You are an AI tasked with classifying documents. Categorize the information into the following: 
+You are an AI specializing in document classification. Extract and categorize information based on the following categories:
 '''
 
 HARDENING_TEMPLATE = '''
-DO NOT include unnecessary text
-DO NOT output the word json
-
-Format dates as YYYY-MM-DD
-Output JSON only.
+- Output only JSON. No extra text.
+- Exclude the word "json" in output.
+- Use YYYY-MM-DD for dates.
+- Map: "First" → First Name, "Last" → Last Name, "Middle" → Middle Name ,"M.I." → Middle Initial.
+- Extract and separate first, middle, and last names.
+- If Middle Name or MI is not mentioned, leave it blank.
+- Correct spelling based on context.
+- If category is not specified, put Not specified.
 '''
 
 RECOMMENDED_DOCUMENTS_LISTS = {
@@ -18,11 +21,11 @@ RECOMMENDED_DOCUMENTS_LISTS = {
     'national certificate':
         '''ISSUING_BODY,POSITION_CERTIFICATE,VALIDITY_DATE,DOCUMENT_NUMBER,DOCUMENT_TYPE''',
     'flag book':
-        '''ISSUING_BODY,VALIDITY_DATE,ISSUING_COUNTRY,PLACE_OF_ISSUE,BOOK_NUMBER,COUNTRY,LAST_NAME,MIDDLE_NAME,FIRST_NAME''',
+        '''ISSUING_BODY,VALIDITY_DATE,ISSUING_COUNTRY,PLACE_OF_ISSUE,BOOK_NUMBER,COUNTRY,LAST_NAME,FIRST_NAME,MIDDLE_NAME,''',
     'seamans book': 
         '''ISSUING_BODY,VALIDITY_DATE,ISSUED_DATE,PLACE_OF_ISSUE,BOOK_NUMBER,COUNTRY,LAST_NAME,FIRST_NAME''',
     'visa': 
-        '''ISSUING_BODY,VALIDITY_DATE,ISSUED_DATE,DOCUMENT_NUMBER,PLACE_OF_ISSUE,LAST_NAME,MIDDLE_NAME,FIRST_NAME,VISA_TYPE''',
+        '''ISSUING_BODY,VALIDITY_DATE,ISSUED_DATE,DOCUMENT_NUMBER,PLACE_OF_ISSUE,LAST_NAME,FIRST_NAME,MIDDLE_NAME,VISA_TYPE''',
     'passport': 
-        '''VALIDITY_DATE,ISSUED_DATE,DOCUMENT_NUMBER,PLACE_OF_ISSUE,COUNTRY,LAST_NAME,MIDDLE_NAME,FIRST_NAME'''
+        '''VALIDITY_DATE,ISSUED_DATE,DOCUMENT_NUMBER,PLACE_OF_ISSUE,COUNTRY,LAST_NAME,FIRST_NAME,MIDDLE_NAME,'''
 }
