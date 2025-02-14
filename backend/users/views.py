d619efdca814310797f4e27c2b2917fc72e0f227
Patrick Gallardo <mooltoo.bot@gmail.com>
2025-02-14 13:18:08 +0800

    Updated formatting

--- backend/users/views.py
+++ backend/users/views.py
@@ -11,24 +11,30 @@ import json
 
 # Create your views here.
 
+
 def index(request):
-    return redirect('/users/login')
+    return redirect("/users/login")
+
 
 def login_page(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         data = json.loads(request.body)
         serializer = LoginSerializer(data=data)
         if serializer.is_valid():
             user = serializer.validated_data
             login(request, user)
-            return JsonResponse({'message': f'{STATUS_MESSAGES["success"]["LOGIN_SUCCESSFUL"]}'}, 
-                            status=STATUS_CODES["success"][200])
+            return JsonResponse(
+                {"message": f'{STATUS_MESSAGES["success"]["LOGIN_SUCCESSFUL"]}'},
+                status=STATUS_CODES["success"][200],
+            )
         else:
-            return JsonResponse({'message': f'{STATUS_MESSAGES["errors"]["INVALID_CREDENTIALS"]}'}, 
-                            status=STATUS_CODES["errors"][401])
-    return render(request, 'index.html')
+            return JsonResponse(
+                {"message": f'{STATUS_MESSAGES["errors"]["INVALID_CREDENTIALS"]}'},
+                status=STATUS_CODES["errors"][401],
+            )
+    return render(request, "index.html")
 
 
 @login_required
 def docs_page(request):
-    return SpectacularSwaggerView.as_view(url_name='schema')(request)
+    return SpectacularSwaggerView.as_view(url_name="schema")(request)
