8447dd094e93de26c9e692d2d51cdf2c90dbd38d
Patrick Gallardo <mooltoo.bot@gmail.com>
2025-02-14 13:14:58 +0800

    Created model for storing openai logs to db

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+class OpenAIRequest(models.Model):
+    document_type = models.CharField(max_length=225)
+    ai_model = models.CharField(max_length=50)
+    prompt_tokens = models.IntegerField()
+    completion_tokens = models.IntegerField()
+    total_tokens = models.IntegerField()
+    created_at = models.DateTimeField(auto_now=True)
