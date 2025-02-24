05cb029f5d89f79377cd06b0d6c431ba41c4f17e
Patrick Gallardo <mooltoo.bot@gmail.com>
2025-02-24 10:08:18 +0800

    Remove threading of ocr

--- backend/api/utils/document_parsing.py
+++ backend/api/utils/document_parsing.py
@@ -46,7 +46,7 @@ def download_file(file_url: str):
 
 def process_page(page):
     """Process a single page using OCR and extract text."""
-
+    print("PROCESSING PAGE...")
     page = page.resize((int(page.width * 0.75), int(page.height * 0.75)))  # Resize to double the size
     img = cv2.cvtColor(np.array(page), cv2.COLOR_RGB2GRAY)  # Convert to grayscale
     result = ocr.ocr(img, cls=True)
@@ -63,20 +63,17 @@ def doc_parse(file: BinaryIO):
     start_time = time.time()
     pdf_bytes = file.read()
 
-    extracted_text = []
-    num_workers = min(2, max(1, os.cpu_count() // 2))
-
-    print("START DOCUMENT PROCESSING...")
 
     # Convert PDF to images (Lower DPI to speed up conversion)
     all_pages = convert_from_bytes(pdf_bytes, dpi=100, poppler_path=POPPLER_PATH)
 
-    with ProcessPoolExecutor(max_workers=num_workers) as executor:
-        extracted_text = list(executor.map(process_page, all_pages))
-    # with ThreadPoolExecutor(max_workers=num_workers) as executor:
-    #     extracted_text = list(executor.map(process_page, pages))
+    print("START DOCUMENT PROCESSING...")
+    # Process only the first page
+    extracted_text = process_page(all_pages[0]) if all_pages else ""
+
     print("EXTRACTED TEXT: ", extracted_text)
     end_time = time.time()
     print(f"Document parsing took {end_time - start_time:.2f} seconds.")
 
-    return " ".join(extracted_text)
+    return extracted_text
+
