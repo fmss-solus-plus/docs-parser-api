d76cbb29906d4f6ce0300adf327fc780dd8fa2e6
Patrick Gallardo <mooltoo.bot@gmail.com>
2025-02-28 10:14:12 +0800

    Refactored ocr config now uses paddlev3

--- backend/api/utils/document_parsing.py
+++ backend/api/utils/document_parsing.py
@@ -7,7 +7,6 @@ from backend.status_code import STATUS_CODES, STATUS_MESSAGES
 from backend.settings import POPPLER_PATH
 
 import numpy as np
-import cv2
 import requests
 import time
 import gc
@@ -16,6 +15,8 @@ import sys
 ocr = None
 if "makemigrations" not in sys.argv and "migrate" not in sys.argv:
     ocr = PaddleOCR(
+        det_model_dir="api/utils/ai_models/en_PP-OCRv3_det_infer",  # Use English OCR model
+        rec_model_dir="api/utils/ai_models/en_PP-OCRv3_rec_infer", 
         use_angle_cls=False,
         lang="en",
         rec_algorithm="CRNN",
@@ -23,7 +24,6 @@ if "makemigrations" not in sys.argv and "migrate" not in sys.argv:
         det_db_unclip_ratio=1.5,
         use_gpu=False  # Enable GPU acceleration
     )
-
 def download_file(file_url: str):
     try:
         response = requests.get(file_url, timeout=3)
