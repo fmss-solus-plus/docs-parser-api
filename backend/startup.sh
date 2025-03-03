89058f0a13b8e4dac17a6cf4b93dde5209084a29
Patrick Gallardo <mooltoo.bot@gmail.com>
2025-03-03 15:44:42 +0800

    Added installation of libraries

--- backend/startup.sh
+++ backend/startup.sh
@@ -2,6 +2,11 @@
 
 cd backend
 
+# Check if Django is installed in the virtual environment
+if ! python -c "import django" &> /dev/null; then
+    pip install -r requirements.txt
+fi
+
 python manage.py migrate
 
-gunicorn backend.wsgi:application 
+gunicorn backend.wsgi:application
