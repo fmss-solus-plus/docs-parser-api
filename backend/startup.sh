6eeedc1eb4fb287cd5c59fe8039a23ee247c380e
Patrick Gallardo <mooltoo.bot@gmail.com>
2025-03-04 07:47:13 +0800

    Updated startup.sh and remove files

--- backend/startup.sh
+++ backend/startup.sh
@@ -1,7 +1,4 @@
-#!/bin/bash
-
-cd backend
-
-python manage.py migrate
-
-gunicorn backend.wsgi:application
+#!/bin/sh
+export PATH="$HOME/antenv/bin:$PATH"
+cd backend  # Change to the directory containing manage.py
+gunicorn --workers=1 --bind=0.0.0.0:8000 backend.wsgi
