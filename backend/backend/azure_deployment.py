939b478af9cfe1d65dce522c9df69548394523f1
Patrick Gallardo <mooltoo.bot@gmail.com>
2025-03-03 09:13:44 +0800

    Installed whitenoise and used in middleware

--- backend/backend/azure_deployment.py
+++ backend/backend/azure_deployment.py
@@ -9,6 +9,21 @@ ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
 CSRF_TRUSTED_ORIGINS = ['https://' + os.environ['WEBSITE_HOSTNAME']]
 DEBUG = False
 
+MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
 conn_string = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
 conn_string_params = {pair.split("="):pair.split("=")[1] for pair in conn_string.split(' ')}
 
